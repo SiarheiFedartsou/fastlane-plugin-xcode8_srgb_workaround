41746c2abeea3f65d55bda6197a6d517b3a53ff5
Siarhei Fiedartsou <Sergei_Fedortsov@cedoni.com>
2016-10-15 23:21:25 +0300

    fixed to convert JPEGs too

--- lib/fastlane/plugin/xcode8_srgb_workaround/actions/apply_xcode8_srgb_workaround_action.rb
+++ lib/fastlane/plugin/xcode8_srgb_workaround/actions/apply_xcode8_srgb_workaround_action.rb
@@ -2,16 +2,19 @@ module Fastlane
   module Actions
     class ApplyXcode8SrgbWorkaroundAction < Action
       def self.run(params)
-        for subdirectory in params[:subdirectories]
-          pattern = File.expand_path(subdirectory) + '/*.png'
-          for png in Dir[pattern]
-            `sips -m '/System/Library/Colorsync/Profiles/sRGB Profile.icc' '#{png}' --out '#{png}'`
-          end
-        end
+        params[:subdirectories].each { |subdirectory|
+          ['png', 'jpg', 'jpeg'].each { |fmt|
+            pattern = File.expand_path(subdirectory) + "/*.#{fmt}"
+            Dir[pattern].each { |resource_path|
+                `sips -m '/System/Library/Colorsync/Profiles/sRGB Profile.icc' '#{resource_path}' --out '#{resource_path}'`
+            }
+          }
+
+        }
       end
 
       def self.description
-        "Converts PNGs in your project to sRGB format to avoid crashes when building with Xcode 8 for iOS 8 and earlier deployment target"
+        "Converts PNGs and JPEGs in your project to sRGB format to avoid crashes when building with Xcode 8 for iOS 8 and earlier deployment target"
       end
 
       def self.authors
@@ -22,9 +25,9 @@ module Fastlane
         [
 
           FastlaneCore::ConfigItem.new(key: :subdirectories,
-                                description: "List of subdirectories where PNGs must be detected and converted to sRGB",
+                                description: "List of subdirectories where PNGs and JPEGs must be detected and converted to sRGB",
                                 default_value: ['./**'],
-                                      type: Array)                         
+                                      type: Array)
         ]
       end
 
